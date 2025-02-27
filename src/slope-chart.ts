4e2330febc67a11658c541223008d8380482894f
Alessandro <alessandropeci@webmapp.it>
2025-02-27 12:00:06 +0100

    chore: Add WmSlopeChartFlowLineQuote interface
    This commit adds the WmSlopeChartFlowLineQuote interface to the slope-chart.ts file. This interface includes properties for flow line quotes in different colors and an HTML string.

--- src/slope-chart.ts
+++ src/slope-chart.ts
@@ -1,8 +1,14 @@
-import { LineString } from 'geojson';
-import { Location, WmFeature } from './feature';
+import {LineString} from 'geojson';
+import {Location, WmFeature} from './feature';
 
 export interface ISlopeChartStyle {
   backgroundColor: string;
+}
+
+export interface WmSlopeChartFlowLineQuote {
+  flow_line_quote_orange: number;
+  flow_line_quote_red: number;
+  html: string;
 }
 
 export interface WmSlopeChartHoverElements {
