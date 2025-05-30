433097493861024a435d991621d3f01c86fa7ea8
Alessandro <alessandropeci@webmapp.it>
2025-05-30 11:57:04 +0200

    chore(elastic): ✨ add new TypeScript interfaces for Elastic search responses
    This commit introduces a new TypeScript file `elastic.ts` that defines interfaces for handling Elastic search responses. The interfaces include:
    
    - `Elastic`: A generic interface allowing dynamic properties.
    - `Hit`: Represents individual search results with fields such as `cai_scale`, `distance`, `feature_image`, and more, capturing the essential properties of each hit.
    - `Bucket` and `Count`: Interfaces defining structures for aggregations, useful for handling grouped search results.
    - `AggregationDetail` and `Aggregations`: Interfaces that organize detailed aggregation results, including themes, activities, and layers.
    - `Response`: The main interface for an Elastic search response, comprising `aggregations` and `hits`.
    
    These interfaces are designed to provide a strong type-checking mechanism for handling complex Elastic search data structures in TypeScript applications.

---
+++ src/elastic.ts
@@ -0,0 +1,47 @@
+export interface Elastic {
+  [name: string]: any;
+}
+
+export interface Hit {
+  cai_scale: string;
+  distance: string;
+  feature_image: string | any;
+  id: number | string;
+  layers: number[];
+  name: string;
+  properties: {[key: string]: any};
+  ref: string;
+  size?: any;
+  taxonomyActivities: any;
+  taxonomyWheres: string[];
+  start?: number[];
+  end?: number[];
+  distanceFromCurrentLocation?: number;
+}
+
+export interface Bucket {
+  key: string | number;
+  doc_count: number;
+}
+
+export interface Count {
+  doc_count_error_upper_bound: number;
+  sum_other_doc_count: number;
+  buckets: Bucket[];
+}
+
+export interface AggregationDetail {
+  doc_count: number;
+  count: Count;
+}
+
+export interface Aggregations {
+  themes: AggregationDetail;
+  activities: AggregationDetail;
+  layers: AggregationDetail;
+}
+
+export interface Response {
+  aggregations: Aggregations;
+  hits: Hit[];
+}
