75de8df782a3aac2e64a797599568b86e583e1a6
Rubens Garofalo <rubensgiuseppegarofalo@gmail.com>
2025-11-06 21:31:00 +0100

    refactor: reorganize config interfaces oc: 6540
    Reintroduces and expands configuration interfaces to enhance
    modular structure and maintainability, focusing on application
    and feature settings.
    
    Relates to oc_6540

--- src/config.ts
+++ src/config.ts
@@ -6,3 +6,18 @@ export interface ZoomFeaturesInViewport {
 export interface ICONS {
   [key: string]: string;
 }
+
+export interface APP {
+  androidStore?: string;
+  customerName?: string;
+  forceToReleaseUpdate?: boolean;
+  geohubId?: number;
+  googlePlayUrl?: string;
+  id?: string;
+  iosStore?: string;
+  sku?: string;
+  name: string;
+  poi_acquisition_form?: any;
+  track_acquisition_form?: any;
+  welcome?: string;
+}
